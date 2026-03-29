823f39fa0f6671b1897539b4833e948cfcd6db1e
ivaraz <rifa.bestrong@gmail.com>
2026-03-30 00:55:24 +0700

    Update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { weddingData } from "../data/wedding";
 
 interface NavbarProps {
   isVisible: boolean;
